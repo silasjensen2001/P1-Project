587c8540ab7576d4c15f6244bfa43305d19705d1
thor2643 <Thor951753@hotmail.com>
2022-11-02 12:52:01 +0100

    Revert "Revert "teste""
    This reverts commit 4cedb6f8a13bea9ac91c47d1e63e371dc04f1205.

---
+++ Zumo/Zumo.ino
@@ -0,0 +1,48 @@
+//The main program where all header files are merged
+#include <Wire.h>
+#include <Zumo32U4.h>
+#include "functions.h"
+
+
+
+Zumo32U4ButtonA But_A;
+Zumo32U4ButtonB But_B;
+Zumo32U4ButtonC But_C;
+
+Rockxanne Rockxan;
+
+float sten[4][2] = {{80,80},{10,80},{10,10},{50,50}};
+
+
+void setup(){
+    Serial.begin(9600);
+    Rockxan.init();
+
+}
+
+void loop(){
+    if (But_A.isPressed()){
+        int i = 0;
+        delay(500);
+
+        while(i < 4){
+            Rockxan.koortilkordinat(sten[i], 25);
+            delay(500); 
+            i++;
+        }       
+    }
+
+    if (But_B.isPressed()){
+        delay(500);
+        Rockxan.reset();
+    }
+
+    if (But_C.isPressed()){
+        delay(700);
+        Rockxan.reset();
+        Rockxan.drive_straight(200, 20);
+        delay(500);
+        Rockxan.turn_to(-180);
+        Rockxan.drive_straight(200, 20);
+    }
+}
