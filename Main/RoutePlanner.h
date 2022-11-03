ca9688a25b5546823215da24729608f3f71a553a
thor2643 <Thor951753@hotmail.com>
2022-11-03 13:56:29 +0100

    3 headers er tilføjet

---
+++ Main/RoutePlanner.h
@@ -0,0 +1,23 @@
+//This header file contains the "Rockxanne" class with its respective attributes and methods
+//This is a comment for testing GIT :O
+//First a few libraries need to be imported
+
+#include "arduino.h"
+#include <Wire.h>
+#include <Zumo32U4.h>
+#include "ZumoDrive.h"
+
+
+
+//The Rockxanne class includes all useful methods for steering the zumo robot
+//Its attributes keep track of variables like current angle and position
+class RoutePlanner{
+
+    //Protected er ligesom private blot at hovedeklassen har adgang til dem
+    protected:
+
+    //Herunder placeres alle metoderne/funktionerne 
+    public:
+
+
+};
