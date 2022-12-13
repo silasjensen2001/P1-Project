56374ab4c8f7a35ff6c2cb10dcb1292eec268495
thor2643 <Thor951753@hotmail.com>
2022-12-13 15:21:46 +0100

    virker ikke

--- Main/Main.ino
+++ Main/Main.ino
@@ -16,7 +16,7 @@ float values[9][2] = {{0.5, 0.5}, {2, 0.5}, {5, 0.5}, {10, 0.5}, {15, 0.5}, {0.5
 
 void setup(){
     Serial.begin(9600);
-    Rockxan.init_all("LCD");  //If its the OLED version write that, else write LCD.
+    Rockxan.init_all("OLED");  //If its the OLED version write that, else write LCD.
     Rockxan.display_print("Klar");
 }
 
