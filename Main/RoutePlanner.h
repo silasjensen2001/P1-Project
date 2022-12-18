3b0112fc22d54d03fd34c3417d95d3f3f72b64df
ShadowWarlord12 <54105795+ShadowWarlord12@users.noreply.github.com>
2022-12-18 15:45:55 +0100

    Til Thor

--- Main/RoutePlanner.h
+++ Main/RoutePlanner.h
@@ -17,14 +17,16 @@ class RoutePlanner{
     float length;
 
     int index;
-    int list_size = 4; 
+    int list_size = 5; 
+    int count = 0;
+    int counts = 0;
 
-    float length_list[4];
-    float route_list[4][2];
-    float current_pos[2];       //{x,y} [cm] 
+    float length_list[5];
+    int route_list[5][2] = {{0,0},{0,0},{0,0},{0,0},{0,0}};
+    int current_pos[2];       //{x,y} [cm] 
 
-    int index_list[4];
-    int stone_list[4][2] = {{70, 15},{20, 40},{70,60},{50,10}};
+    int index_list[5];
+    int stone_list[5][2] = {{0,0},{70,15},{20,40},{70,60},{50,10}};
     
          
 
@@ -73,6 +75,147 @@ class RoutePlanner{
             }
         }
         
+        // Farthest Insertion function
+        void farthest_insertion(){
+            current_pos[0] = 0;
+            current_pos[1] = 0;
+            int check_list[4] = {0,0,0,0,0};
+            float length_total_list[4] = {0,0,0,0,0};
+            float length = sqrt(pow((stone_list[0][0] - current_pos[0]), 2) + pow((stone_list[0][1] - current_pos[1]), 2));
+            float longest_length = 0;
+            int longest_length_index;
+            int shortest_dist_index;
+            float route_dist = 0;
+            float shortest_dist = 1000000;
+            int index = 0;
+            // Find and calculate distance to the farthest point from Zumo
+            for (size_t i = 0; i < list_size; i++){
+                for (size_t j = 0; j < list_size; j++){
+                    if ((length <= (sqrt(pow((stone_list[j][0] - current_pos[0]), 2) + pow((stone_list[j][1] - current_pos[1]), 2))))){ 
+                        length = (sqrt(pow((stone_list[j][0] - current_pos[0]), 2) + pow((stone_list[j][1] - current_pos[1]), 2)));
+                        index = j;
+                    }
+                }
+            }
+            // Place the first two points in the route list
+            route_list[0][0] = current_pos[0];
+            route_list[0][1] = current_pos[1];
+            route_list[1][0] = stone_list[index][0];
+            route_list[1][1] = stone_list[index][1];
+            check_list[0] = 1;
+            check_list[1] = 1;
+            Serial.println(route_list[0][0]);
+            Serial.println(route_list[0][1]);
+            Serial.println(route_list[1][0]);
+            Serial.println(route_list[1][1]);
+            Serial.println("-----");
+            Serial.println(check_list[0]);
+            Serial.println(check_list[1]);
+            Serial.println(check_list[2]);
+            Serial.println(check_list[3]);
+            Serial.println("-----");
+
+            while (count < list_size){
+                // Calculate the distance to all points outside the list to find the point farthest from any point within the list
+                while(counts < list_size){
+                    for (size_t i = 0; i < list_size; i++){
+                        for (size_t j = 0; j < list_size; j++){
+                            if (check_list[j] == 0 && check_list[i] == 1){
+                                length = (sqrt(pow((stone_list[j][0] - route_list[i][0]), 2) + pow((stone_list[j][1] - route_list[i][1]), 2)));
+                                index = j;
+                                length_total_list[j] += length;
+                            }
+                        }
+                        counts += 1;
+                    }
+                    Serial.println(length_total_list[0]);
+                    Serial.println(length_total_list[1]);
+                    Serial.println(length_total_list[2]);
+                    Serial.println(length_total_list[3]);
+                    Serial.println(length_total_list[4]);
+                }
+                // find the longest length and its index
+                for (size_t i = 0; i < list_size; i++){
+                    if (longest_length < length_total_list[i]){
+                        longest_length = length_total_list[i];
+                        longest_length_index = i;
+                    }
+                }
+                Serial.println("-----");
+                Serial.println(longest_length);
+                Serial.println(longest_length_index);
+                Serial.println("-----");
+
+                float length_total_list[4] = {0,0,0,0,0};
+                
+                // Place the new point in all possible locations of the array and calculate the route for each instance.
+                for (size_t i = 0; i < list_size; i++){
+                    if (check_list[i] == 1){
+                        for (size_t t = list_size - 1; t > i; t--){
+                            route_list[t][0] = route_list[t-1][0];
+                            route_list[t][1] = route_list[t-1][1];
+                            check_list[t] = check_list[t-1];
+                        }
+                        route_list[i][0] = stone_list[longest_length_index][0];
+                        route_list[i][1] = stone_list[longest_length_index][1];
+                        check_list[i] = 1;
+                        for (size_t j = 0; j < list_size; j++){
+                            if (check_list[j] == 1 && check_list[j+1] == 1){
+                                length_total_list[i] += (sqrt(pow((route_list[j+1][0] - route_list[j][0]), 2) + pow((stone_list[j+1][1] - route_list[j][1]), 2)));
+                            }
+                        }
+                    }
+                    // Reset and calculate again
+                    float route_list[4][2] = {{0,0},{0,0},{0,0},{0,0}};
+                    float check_list[4] = {0,0,0,0};
+                    route_list[0][0] = current_pos[0];
+                    route_list[0][1] = current_pos[1];
+                    route_list[1][0] = stone_list[index][0];
+                    route_list[1][1] = stone_list[index][1];
+                    check_list[0] = 1;
+                    check_list[1] = 1;
+                }
+
+                // Check the length total list and choose the shortest one
+                for (size_t i = 0; i < list_size; i++){
+                    if (shortest_dist < length_total_list[i]){
+                        shortest_dist = length_total_list[i];
+                        shortest_dist_index = i;
+                    }
+                }
+                // Place the point in its respective place
+                for (size_t j = list_size - 1; j > shortest_dist_index; j--){
+                    route_list[j][0] = route_list[j-1][0];
+                    route_list[j][1] = route_list[j-1][1];
+                }
+                route_list[shortest_dist_index][0] = stone_list[longest_length_index][0];
+                route_list[shortest_dist_index][1] = stone_list[longest_length_index][1];
+                longest_length = 0;
+                longest_length_index = 0;
+                shortest_dist = 0;
+                shortest_dist_index = 0;
+                counts = 0;
+                count += 1;
+            }
+            Serial.println("-----");
+            Serial.println("-----");
+            Serial.println(route_list[0][0]);
+            Serial.println(route_list[0][1]);
+            Serial.println(route_list[1][0]);
+            Serial.println(route_list[1][1]);
+            Serial.println(route_list[2][0]);
+            Serial.println(route_list[2][1]);
+            Serial.println(route_list[3][0]);
+            Serial.println(route_list[3][1]);
+            Serial.println("-----");
+            Serial.println(check_list[0]);
+            Serial.println(check_list[1]);
+            Serial.println(check_list[2]);
+            Serial.println(check_list[3]);
+            Serial.println("-----");
+        }
+
+
 
         //This method sorts the stone coordinates according to the X-axis
         //This is useful when the stone must be picked up using the spray tracks
@@ -97,8 +240,8 @@ class RoutePlanner{
                         update_list[S+1][1] = stone_list[S][1];  
 
                         for(int i = 0; i < list_size; i++){
-                            stone_list[i][0]=update_list[i][0];
-                            stone_list[i][1]=update_list[i][1];
+                            stone_list[i][0] = update_list[i][0];
+                            stone_list[i][1] = update_list[i][1];
                         } 
                     }
                 }  
